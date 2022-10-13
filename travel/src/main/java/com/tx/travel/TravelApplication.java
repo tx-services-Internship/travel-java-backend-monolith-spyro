6775dab8490c3a0011cb8ec7a0b3347a589d7514
Magdalina Civovic <megicivovic@yahoo.com>
2022-10-13 12:29:01 +0200

    suggestions from code review

--- travel/src/main/java/com/tx/travel/TravelApplication.java
+++ travel/src/main/java/com/tx/travel/TravelApplication.java
@@ -3,7 +3,6 @@ package com.tx.travel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** Hello world! */
 @SpringBootApplication
 public class TravelApplication {
   public static void main(String[] args) {
