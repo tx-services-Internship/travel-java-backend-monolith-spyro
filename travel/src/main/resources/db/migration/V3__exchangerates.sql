12adaf4654a26c7d3b719a724a1cd3beb2586ec4
DusanBrankovic <dbrankovic3819rn@raf.rs>
2022-11-23 12:25:40 +0100

    Changed property names in ExchangeRate class and migration file to more accurately represent what info they hold. Changed hibernate property from update to validate.

--- travel/src/main/resources/db/migration/V3__exchangerates.sql
+++ travel/src/main/resources/db/migration/V3__exchangerates.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `exchange_rates` (
     `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-    `currency` VARCHAR(20) DEFAULT NULL,
-    `rate` FLOAT UNSIGNED DEFAULT NULL,
+    `code` VARCHAR(20) DEFAULT NULL,
+    `amountInRsd` FLOAT UNSIGNED DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `UK_currency` (`currency`)
+    UNIQUE KEY `UK_currency` (`code`)
 );
