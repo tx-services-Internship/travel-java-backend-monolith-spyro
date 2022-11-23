da7034d1a05ff097269add4a3308d6e5e62b291e
DusanBrankovic <dbrankovic3819rn@raf.rs>
2022-11-23 13:58:50 +0100

    Renamed ExchangeRateRequest to ExchangeRatePostRequest. Added attributes in ExchangeRatePutRequest.

--- travel/src/main/java/com/tx/travel/payload/request/ExchangeRatePutRequest.java
+++ travel/src/main/java/com/tx/travel/payload/request/ExchangeRatePutRequest.java
@@ -1,2 +1,12 @@
-package com.tx.travel.payload.request;public class ExchangeRatePutRequest {
+package com.tx.travel.payload.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ExchangeRatePutRequest {
+
+    private final String code;
+    private final Float amountInRsd;
 }
