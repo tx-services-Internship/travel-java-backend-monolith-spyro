84af82ec6917cb33b0ecfc8c0b26e473009a82ab
DusanBrankovic <dbrankovic3819rn@raf.rs>
2022-11-23 14:36:26 +0100

    Revert "Changed property names in ExchangeRate class and migration file to more accurately represent what info they hold. Changed hibernate property from update to validate."
    This reverts commit 0dce1a68

--- travel/src/main/java/com/tx/travel/model/ExchangeRate.java
+++ travel/src/main/java/com/tx/travel/model/ExchangeRate.java
@@ -1,25 +1,16 @@
 package com.tx.travel.model;
 
 import jdk.jfr.Unsigned;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-
 @Entity
 @Table(name = "exchange_rates",
         uniqueConstraints = {
-            @UniqueConstraint(columnNames = "code")
+            @UniqueConstraint(columnNames = "currency")
 })
 public class ExchangeRate {
 
@@ -30,7 +21,7 @@ public class ExchangeRate {
     @NotBlank
     @NotNull
     @Length(max = 20)
-    private String code;
+    private String currency;
 
     @NotNull
     private Float amountInRsd;
